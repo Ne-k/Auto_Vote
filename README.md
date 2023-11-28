46df798e517e344ada26b9b7a73dfd71dae8cb9e
npspy <username.lilrice@gmail.com>
2023-11-27 21:15:49 -0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Disclaimer
+**This project is for educational purposes and I, the developer, am not responsible for who or what happens with this script.** This was mostly for cybersecurity research purposes on my end. 
+
+
 # How to use
 1) In the `Auto_Vote.py` you'll have to change this to `driver.get("<Your Voting page URL>")` with your URL to who you want to vote for the CampusTech giveaway. For the script to work correctly, you'll need to use a `https://campustech.org/tech-the-halls?entry_id=xxx` URL, you can get that URL by clicking the share button on whoever you want to spam vote. 
 2) At the bottom of `Auto_Vote.py`, there's these couple lines, where it says `for _ in range(100)`, change the 100 to whatever number of instances you want to run. If you choose a high number like 100 and stop the script mid-way, the script will essentially be a virus and you'll see why if that happens to you, so don't stop the script mid-way. 
--- spam_email.py
+++ spam_email.py
@@ -3,23 +3,7 @@ import json
 
 url = "https://campustech.org/api/2.0/contacts/send/email/user"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
-    "Accept": "*/*",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Content-Type": "application/json",
-    "Origin": "https://campustech.org",
-    "DNT": "1",
-    "Sec-GPC": "1",
-    "Connection": "keep-alive",
-    "Referer": "https://campustech.org/tech-the-halls",
-    "Cookie": 'permanent_cookie={"id":"FBC5462D-83CF-4535-BA4A-A15969F2EB35"}; user_search_params={}; '
-              'session_cookie={"id":"101E927E-D6D0-49A6-B9F0-196CC6844446"}; '
-              'campustech_org.sid=s%3AXYdhKmeVndj9KNkSielawVWSJlrCyj7E.Jqth9EVhT8KnmMsatX21KbNy5ydjr41Q2zXSc2tjA5s',
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "TE": "trailers"
+
 }
 payload = {
     "subject": "Verification Code Email",
